c723bc7b9b815485f2e5f08c56bcd52558eea62a
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-08-23 13:47:23 -0500

    update

---
+++ Week4/Day1/assignments.js
@@ -0,0 +1,25 @@
+// Write a madlib function, which is given a name and a subject. 
+// It will return(not print) a new string: (name)'s favorite subject in 
+// school is (subject)
+
+// #1
+function madlib (name, subject){
+    return console.log((name,"favorite subject in school is", subject))
+}
+
+madlib ("ash" , "math")
+
+//Write a function tipAmount that is given the bill amount and the level of 
+// service (one of good, fair and poor) and returns the dollar amount for the tip. Based on:
+// good -> 20% fair -> 15% bad -> 10%
+
+// #2
+
+function tipAmount(){
+
+    let billAmount = $("#bill").value;
+    let tipAmount = billAmount *
+    
+
+}
+
---
+++ Week4/Day1/introToJavascript.js
@@ -0,0 +1,3 @@
+// print Write a madlib function, which is given a name and a subject. 
+// It will return(not print) a new string: (name)'s favorite subject in 
+// school is (subject)
