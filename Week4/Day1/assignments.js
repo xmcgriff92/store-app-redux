3f38d557c271e4419bae98b2fdff9b46775cc720
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-08-23 14:21:47 -0500

    Update assignments.js

--- Week4/Day1/assignments.js
+++ Week4/Day1/assignments.js
@@ -15,11 +15,54 @@ madlib ("ash" , "math")
 
 // #2
 
-function tipAmount(){
+const good = .2
+const fair = .15
+const poor = .1
 
-    let billAmount = $("#bill").value;
-    let tipAmount = billAmount *
-    
+function tipAmount(billAmount, tipAmount){
 
+    let tip = billAmount * tipAmount
+    return console.log(tipAmount)
 }
 
+tipAmount(40, fair)
+
+// Write a function totalAmount that takes the same arguments as tipAmount except it
+ // returns the total as the tip amount plus the bill amount. This function may make
+ // use of tipAmount as a sub-task.
+
+ // #3 
+
+
+function totalAmount (billAmount, tipAmount){
+    let tip = billAmount * tipAmount
+    let total = billAmount + tipAmount
+    return console.log(total)
+}
+
+totalAmount(10, good)
+
+// Write a function printNumbers which is given a start number and an end number.
+// It will print the numbers from one to the other, one per line:
+
+// #4
+
+let arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// function printNumbers(){
+    //for (arrayOfNumbers = 1; arrayOfNumbers <= 10; arrayOfNumbers++) {
+       // console.log(arrayOfNumbers)
+    //}
+//}
+
+// printNumbers()
+
+function printNumbers(){
+    arrayOfNumbers = 1
+    while (arrayOfNumbers <= 10) {
+        console.log(arrayOfNumbers);
+        arrayOfNumbers++;
+    }
+}
+
+printNumbers()
