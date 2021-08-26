cde104514269c8ca3ad15ae7ad8f64ccd20ed0aa
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-08-26 18:13:49 -0500

    Update movieApp.js

--- Week4/Day4/movieApp.js
+++ Week4/Day4/movieApp.js
@@ -1,4 +1,11 @@
-
+// header for movie application
 const header =document.createElement("h1")
-header.innerHTML= "Movie app"
+header.innerHTML= "The Movie Searcher"
 document.body.append(header)
+
+// creating main container as well as class and id within DOM
+const div1 =document.createElement("div");
+document.body.append(div1)
+div1.id = "main-container";
+// creating input and button for app within div1.
+const input =document.createElement("input")
