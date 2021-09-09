a01d9e5cdfa2518d5ac9ec764c77c9871c8d5990
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-09-09 00:28:26 -0500

    update

--- Week6/Day2/exercise2.js
+++ Week6/Day2/exercise2.js
@@ -1,4 +1,4 @@
-const PORT = 3002;
+const PORT = 1992;
 const express = require("express");
 const app = express();
 
@@ -6,7 +6,7 @@ const app = express();
     res.send('Node and express are just lovely');
 }) */
 
-const cities = [
+/* const cities = [
     
     { city: "Chicago", continent: "North America" },
     { city: "Boston", continent: "North America" },
@@ -19,13 +19,13 @@ const cities = [
     { city: "Los Angeles", continent: "North America" },
     { city: "Houston", continent: "North America" }
 
-   ];
+   ]; */
 
 
 const es6Renderer = require("express-es6-template-engine");
 
 
-app.engine("html", es6Renderer);
+/* app.engine("html", es6Renderer);
 app.set("views", "templates");
 app.set("view engine", "html");
 
@@ -35,7 +35,7 @@ app.get('/', (req,res) => {
             cities: cities
         },
     });
-});
+}); */
 
 //
 ////
@@ -51,6 +51,28 @@ app.get('/', (req,res) => {
 }) */
 
 
+const movies = {
 
+    movieName: "Training Day", poster: "https://m.media-amazon.com/images/I/816XrpA0VfS._AC_SY679_.jpg",
+    movieName: "Pulp Fiction", poster: "https://cdn.shopify.com/s/files/1/1057/4964/products/pulp-fiction-vintage-movie-poster-original-1-sheet-27x41-6_300x@2x.jpg?v=1607050941",
+    movieName: "Snakes on a plane", poster: "https://m.media-amazon.com/images/I/512I2n3duwL._AC_.jpg",
+    movieName: "Clash of the titans", poster: "https://movieposters2.com/images/702300-b.jpg",
+    movieName: "The 40 year old virgin", poster: "https://m.media-amazon.com/images/I/51F1M8eR+mL._AC_.jpg"
+
+}
+
+app.engine("html", es6Renderer);
+app.set("views", "templates");
+app.set("view engine", "html");
+
+app.get('/', (req,res) => {
+    res.render("two", {
+        locals: {
+        
+            movies: movies
+
+        },
+    });
+});
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`))
