85ebc309b66307771757600f30ae8566c9eecbaf
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-09-21 12:39:39 -0500

    Create algos.js

---
+++ Week7/Day5/algos.js
@@ -0,0 +1,22 @@
+// Write a function to return the maximum occurring character in the input string e.g., if input string is "Test" then function should return 't'.
+// Note: capital letter 'T' and small letter 't' should count as the same.
+
+str1 = "holddogecoinkek"
+// result: o
+str2 = "aahhanotheralgo"
+// result: a
+str3 = "randywhyyyyy"
+// result: y
+
+function getMaxLetter(str){
+
+    let count = 0;
+    ////
+    for (let i = 0; i < str.length; i++) {
+
+}
+
+
+}
+
+getMaxLetter()
