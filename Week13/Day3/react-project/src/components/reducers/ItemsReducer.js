83b7b8ec2bac7326dd2936516b01ac44f6972437
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-10-28 17:18:12 -0500

    Update ItemsReducer.js

--- Week13/Day3/react-project/src/components/reducers/ItemsReducer.js
+++ Week13/Day3/react-project/src/components/reducers/ItemsReducer.js
@@ -2,24 +2,24 @@ const initialData = {
 
     items: [{
 
-        name: "Xbox Series X",
+        name: "Microsoft Xbox Series X",
         image: "https://www.citypng.com/public/uploads/preview/-11589218258unxbb3hzak.png",
         price: "399"
     },
     {
-        name: "Playstation 5",
+        name: "Sony Playstation 5",
         image: "https://i5.walmartimages.com/asr/cb95a164-26a4-4428-8bbb-823420a19b9d.fa5d5e818c6be47fb1f0cd326beb11d5.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
         price: "499"
     },
     {
         name: "iMac",
         image: "https://www.designbust.com/download/431/png/macbook_pro_png_icon512.png",
-        price: "1299"
+        price: "1499"
     },
     {
-        name: "",
-        image: "",
-        price: ""
+        name: "Samsung Galaxy S21",
+        image: "https://ss7.vzw.com/is/image/VerizonWireless/samsung-flagship-smartphone-palette-p3-black-128gb-smg998uzkv?$acc-lg$&hei=350&wid=350",
+        price: "1299"
     },
     {
         name: "",
