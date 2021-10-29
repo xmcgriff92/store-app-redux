a01f31e478479d70edb296c972da855d5fd29208
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-10-29 04:18:15 -0500

    name avatars

--- Week13/Day3/react-project/src/components/contacts/Contacts.js
+++ Week13/Day3/react-project/src/components/contacts/Contacts.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
+import Avatar from "react-avatar";
 
 export const Contacts = (props) => {
-const contact = props.contact;
-    return (
-        <tr>
-        <th scope="col">
-          <div className="custom-control custom-checkbox"></div>
-          <input type="checkbox" className="custom-control-input" />
-        </th>
-        <th scope="row">{contact.id}</th>
-        <td>{contact.name}</td>
-        <td>{contact.phone}</td>
-        <td>{contact.email}</td>
-      </tr>
-    )
-}
-
+  const contact = props.contact;
+  return (
+    <tr>
+      <th scope="col">
+        <div className="custom-control custom-checkbox"></div>
+        <input type="checkbox" className="custom-control-input" />
+      </th>
+      <th scope="row">{contact.id}</th>
+      <td>
+        <Avatar name={contact.name} size="45" round={true} />
+        {contact.name}
+      </td>
+      <td>{contact.phone}</td>
+      <td>{contact.email}</td>
+    </tr>
+  );
+};
