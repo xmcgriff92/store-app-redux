e220f26d80ef4c8f4d46ce5c91a8e2adbec65702
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-10-29 05:32:38 -0500

    update

--- Week13/Day3/react-project/src/components/Website.Style.js
+++ Week13/Day3/react-project/src/components/Website.Style.js
@@ -4,3 +4,8 @@ export const  ContactsContainer = styled.div`
 margin-top: 15px;
 padding: 70px;
 `;
+
+export const  FormContainer = styled.div`
+margin-top: 6px;
+margin-bottom: 6px;
+`;
--- Week13/Day3/react-project/src/components/contacts/AddContact.js
+++ Week13/Day3/react-project/src/components/contacts/AddContact.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { ContactsContainer } from '../Website.Style'
+import { ContactsContainer, FormContainer } from '../Website.Style'
 
 export default function AddContact() {
     return (
         <ContactsContainer>
            <div className="card border-0 shadow">
-               <div className="card-header">Add a Contact</div>
+               <div className="card-header">Add a contact</div>
+               <div className="card-body">
+
+                    <form>
+                        <FormContainer className="form-group">
+                            <input className="form-control" type="text" placeholder="enter your name"/>
+                        </FormContainer>
+
+                        <FormContainer className="form-group">
+                            <input className="form-control" type="text" placeholder="enter your phone-number"/>
+                        </FormContainer>
+
+                        <FormContainer className="form-group">
+                            <input className="form-control" type="text" placeholder="enter your email"/>
+                        </FormContainer>
+                    </form>
+
+               </div>
            </div>
         </ContactsContainer>
     )
